0778a3925a85bb6a82cf221c02d0a0ccec818426
TheJPerson <jhetts@ucsc.edu>
2024-10-16 21:17:58 -0700

    e

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Snake
+// Author: Judah Hetts
+// Date: 10/17/2024
 
 // Constants
 
-// Functions
+//Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+  //Create board (15 x 15)
+  //Create player character (1 tile in size) 
+    //Set player in center of board
+  //Create consumable item on random board tile 
+    //When consumable item is touched it disappears
+    //Add one tile of length to player one opposite end of where they were facing
+  //Allow player to move one tile at a time
+  //Generate new consumable item
+  //If player hits wall or self they lose
+
+
 function myFunction(param1, param2) {
   // some code here
   // return results;
